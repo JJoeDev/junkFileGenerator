2304470310b9b91d325cb7405219e613fdff9426
JJoe <48448023+JJoeDev@users.noreply.github.com>
2023-11-23 16:59:55 +0100

    Added wrong file lol

--- main.zig
+++ main.zig
@@ -1,15 +1,15 @@
 const std = @import("std");
 
-fn ask_user() !i64 {
+fn ask_user() !u16 {
     const stdin = std.io.getStdIn().reader();
     const stdout = std.io.getStdOut().writer();
 
-    var buf: [10]u8 = undefined;
+    var writeBuf: [10]u8 = undefined;
 
-    try stdout.print("Guess a number from 1 to 100: ", .{}); // The try means it could return an error and if so it will be passed to the caller of this function (because of !)
+    try stdout.print("Please input a number from 0 to 65530: ", .{});
 
-    if (try stdin.readUntilDelimiterOrEof(buf[0..], '\n')) |user_input| {
-        return std.fmt.parseInt(i64, user_input, 10); // Parse to 64 bit int in base 10
+    if (try stdin.readUntilDelimiterOrEof(writeBuf[0..], '\n')) |userInput| {
+        return std.fmt.parseInt(u16, userInput, 10); // Parse to 16 bit base 10 unsigned int
     } else {
         return error.InvalidParam;
     }
@@ -18,20 +18,32 @@ fn ask_user() !i64 {
 pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
 
-    // Psudo random number generator with random seed
-    var prand = std.rand.DefaultPrng.init(blk: { // blk is used to create a scope for variables
+    const file = try std.fs.cwd().createFile(
+        "JunkFile.txt",
+        .{ .read = true },
+    );
+    defer file.close();
+
+    var prng = std.rand.DefaultPrng.init(blk: {
         var seed: u64 = undefined;
         try std.os.getrandom(std.mem.asBytes(&seed));
-        break :blk seed; // Break the blk block and use seed as the default value
+        break :blk seed;
     });
+    const random = prng.random();
 
-    const value = prand.random().intRangeAtMost(i64, 1, 100);
-    while (true) {
-        const guess = try ask_user();
-        if (guess == value) {
-            break;
-        }
-        try stdout.print("Too {s}\n", .{if (guess < value) "low" else "high"});
+    const size = try ask_user();
+
+    const allocator = std.heap.page_allocator;
+    const junkBuf = try allocator.alloc(u8, size);
+    defer allocator.free(junkBuf);
+
+    var i: u32 = 0;
+    while (i < size) : (i += 1) {
+        junkBuf[i] = random.intRangeAtMost(u8, 0, 255);
     }
-    try stdout.print("Correct!\n", .{});
+
+    const written = try file.write(junkBuf[0..junkBuf.len]);
+    _ = written;
+
+    try stdout.print("Junk has been written to JunkFile.txt\n\n", .{});
 }
